4e5358c855c85eaf53f0d2a22d177939da5af0e9
Endaxe <94972517+Endaxe@users.noreply.github.com>
2023-12-07 13:21:42 +0100

    first lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class Main extends Canvas implements Runnable {
+
+    public static int WIDTH = 800;
+    public static int HEIGHT = 600;
+    public static String title = "fish!";
+
+    private Fish fish;
+
+
+    public void draw(Graphics g) {
+        fish.draw(g);
+    public Main() {
+        fish = new fish();
+
+
+
+
+        try {
+            fish = ImageIO.read(getClass().getResource("fish.png"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 }
